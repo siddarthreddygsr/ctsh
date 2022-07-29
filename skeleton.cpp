00155b65de946eab11fadb4b3814f9f62495b9e5
siddarthreddygsr <siddarthreddygsr@gmail.com>
2022-07-29 15:58:16 +0530

    removed some features for cross-platform compatibility

--- skeleton.cpp
+++ skeleton.cpp
@@ -18,11 +18,13 @@ static volatile sig_atomic_t jump_active = 0;
 
 int main() {
 
+    
     struct sigaction s;
     s.sa_handler = sigint_handler;
     sigemptyset(&s.sa_mask);
     s.sa_flags = SA_RESTART;
     sigaction(SIGINT, &s, NULL);
+
     setenv("SHELL","cybertrauma's shell",1); // does overwrite
     char *user=getenv("USER");
     char* hostname;
@@ -36,7 +38,7 @@ int main() {
     using_history(); 
     read_history("ctsh_history");
     // cout << hostname;
-    string prompt;
+    string prompt= "";
     prompt.append(user);
     prompt.append("@");
     prompt.append(hostname);
@@ -44,17 +46,22 @@ int main() {
     
     while(1)
     {
+        string input;
+        string command[100];
         if (sigsetjmp(env, 1) == 42) {
             printf("\n");
             continue;
         }
         jump_active = 1;
-        string input;
-        string command[100];
+        
         
         input = readline(prompt.c_str());
+        if (input.c_str() == NULL) {  /* Exit on Ctrl-D */
+            printf("\n");
+            exit(0);
+        }
         add_history(input.c_str());
-        append_history(1,"ctsh_history");
+        // append_history(1,"ctsh_history");
         int counter = 0;
         int flag = 0;
         for (short i = 0; i<input.length(); i++){
@@ -90,10 +97,23 @@ int main() {
         child_pid = fork();
         // cout << "childpid " <<child_pid << endl;
         if (child_pid == 0) {
+            struct sigaction s_child;
+            s_child.sa_handler = sigint_handler;
+            sigemptyset(&s_child.sa_mask);
+            s_child.sa_flags = SA_RESTART;
+            sigaction(SIGINT, &s_child, NULL);
             /* Never returns if the call is successful */
             
+            // if (strcmp(argv[0], "cd") == 0) {
+            //     cd(argv[1]);
+            // }
             if (strcmp(argv[0], "cd") == 0) {
-                cd(argv[1]);
+                if (cd(argv[1]) < 0) {
+                    perror(argv[1]);
+                }
+
+                /* Skip the fork */
+                continue;
             }
             else if(strcmp(argv[0],"history") == 0)
             {
@@ -128,19 +148,19 @@ void sigint_handler(int signo) {
 
 void history()
 {
-   HISTORY_STATE *myhist = history_get_history_state ();
+//    HISTORY_STATE *myhist = history_get_history_state ();
 
-    /* retrieve the history list */
-    HIST_ENTRY **mylist = history_list ();
+//     /* retrieve the history list */
+//     HIST_ENTRY **mylist = history_list ();
 
-    for (int i = 0; i < myhist->length; i++) { /* output history list */
-        printf (" %8s  %s\n", mylist[i]->line, mylist[i]->timestamp);
-        free_history_entry (mylist[i]);     /* free allocated entries */
-    }
-    putchar ('\n');
+//     for (int i = 0; i < myhist->length; i++) { /* output history list */
+//         printf (" %8s  %s\n", mylist[i]->line, mylist[i]->timestamp);
+//         free_history_entry (mylist[i]);     /* free allocated entries */
+//     }
+//     putchar ('\n');
 
-    free (myhist);  /* free HIST_ENTRY list */
-    free (mylist);  /* free HISTORY_STATE   */
+//     free (myhist);  /* free HIST_ENTRY list */
+//     free (mylist);  /* free HISTORY_STATE   */
 }
 int cd(char *path)
 {
